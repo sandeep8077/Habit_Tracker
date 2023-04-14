91b580ced2437d50832de09f83e437dd1743605a
Sandeep <sk557618269@gmail.com>
2023-04-14 19:46:48 +0530

    install env package

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const env = require('./environment');
 const mongoose = require("mongoose");
+require("dotenv").config()
 mongoose.connect(process.env.db).then(() => { console.log("Database connected succesfully") }).
 catch((err) => {
     console.log("Not connected database");
